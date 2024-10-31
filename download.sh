5cf5ef9a372f7cc79f798f2e96d4334175edbfb9
hsinhochien <165622803+hsinhochien@users.noreply.github.com>
2024-10-31 16:40:55 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 FOLDER_ID="" # Replace it with the model parameters folder from your Google Cloud.
 
 # 定義下載後存放的目錄
-MODEL_DIR="./my_model_test"
+MODEL_DIR="./my_model"
 
 # 檢查第一個模型目錄是否存在，不存在則創建
 if [ ! -d "$MODEL_DIR" ]; then
@@ -12,7 +12,7 @@ if [ ! -d "$MODEL_DIR" ]; then
 fi
 
 # 下載Google雲端硬碟資料夾的所有內容
-echo "Downloading paragraph selection model parameters from Google Drive..."
+echo "Downloading model parameters from Google Drive..."
 gdown --folder "$FOLDER_ID" -O "$MODEL_DIR"
 
 echo "Model parameters downloaded to $MODEL_DIR"
